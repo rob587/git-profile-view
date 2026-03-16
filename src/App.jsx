e8abd4dd7d2a21078619e64f6dd8a6e7a49ba255
rob587 <robertocamm02@yahoo.com>
2026-03-16 11:25:10 +0100

    Implement Git Profile Finder functionality and remove unused CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+// Profilo utente
+// https://api.github.com/users/USERNAME
+
+import { useState } from "react";
+
+// Repository dell'utente
+// https://api.github.com/users/USERNAME/repos?sort=updated&per_page=6
+
 function App() {
-  return <></>;
+  const [inputValue, setInputValue] = useState("");
+  const [profileDatas, setProfileDatas] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchProfile() {
+    const userFetch = fetch(`https://api.github.com/users/${inputValue}`);
+    const userRepos = fetch(
+      `https://api.github.com/users/${inputValue}/repos?sort=updated&per_page=6`,
+    );
+
+    const [userResponse, userReposResponse] = await Promise.all([
+      userFetch,
+      userRepos,
+    ]);
+
+    const [profileData, reposData] = await Promise.all([
+      userResponse.json(),
+      userReposResponse.json(),
+    ]);
+  }
+
+  return (
+    <>
+      <h1>Git Profile Finder</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
